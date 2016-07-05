57adbec91d30f12bfe7ba0a9b48d0d1f0c7f9f20
Aakash Sharma <aakash.vs13@gmail.com>
2016-07-05 17:59:46 +0530

    final

---
+++ final/angular-rest-hib-spring/WebContent/resources/theme1/js/app/app.js
@@ -0,0 +1,59 @@
+/**
+ *  app
+ */
+
+'use strict';
+
+var App = angular.module('myApp',["ngRoute"]);
+
+App.config(function($routeProvider){
+	
+	$routeProvider
+		.when("/",{
+			templateUrl:"login.jsp",
+			controller:"LoginController",
+			controllerAs: "ctrl"
+		})
+		.when("/teams",{
+			templateUrl:"team.jsp",
+			controller:"TeamController",
+			controllerAs: "ctrl",
+			authenticated:true
+		})
+		.when("/ptalloc",{
+			templateUrl:"project.jsp",
+			controller:"ProjectController",
+			controllerAs:"pctrl",
+			authenticated:true
+		})
+		.when("/admin",{
+			templateUrl:"admin.jsp",
+			controller:"ResourceController",
+			controllerAs: "ctrl",
+			authenticated:true
+			
+		})
+		.otherwise({
+            redirectTo: "/"
+        })
+		
+})
+
+App.directive('checkPermissions', ['PermissionsServices', function(PermissionsServices) {
+    return {
+        restrict: 'A',
+        link: function(scope, elem, attrs, ctrl) {
+
+            if (attrs.permissions != '') {
+                var hasPermission = PermissionsServices.hasAccess(attrs.checkPermissions);
+
+                if (false == hasPermission) {
+                    elem.remove();
+                }
+            } else {
+                elem.remove();
+            }
+        }
+    };
+}]);
+
